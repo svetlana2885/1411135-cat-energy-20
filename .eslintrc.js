20b89f1beb2bb5fb7e46acd2e669ad3cb6cb352e
Света <sveta@mac198RDS.local>
2020-07-19 22:13:41 +0300

    разметка личного проекта

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+};
